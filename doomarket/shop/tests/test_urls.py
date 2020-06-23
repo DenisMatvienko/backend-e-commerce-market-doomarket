e97fb324f485d087ced26df55bcb350f4831bd03
Denis <grabsomebuds27@gmail.com>
2020-06-23 18:06:04 +0300

    tests add

--- doomarket/shop/tests/test_urls.py
+++ doomarket/shop/tests/test_urls.py
@@ -1,10 +1,18 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from shop.views import ProductListView, ProductDetailView
+from shop.views import *
 
 
 class TestUrls(SimpleTestCase):
     """ Url-Product-list-Test """
+
     def test_list_url_is_resolved(self):
         url = reverse('list')
-        self.assertEquals(resolve(url).func, ProductListView)
+        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ProductListView)
+
+    def test_detail_url_resolved(self):
+        """ Url-Product-detail-Test """
+
+        url = reverse('product-detail', args=['slug-slug-sluggy'])
+        self.assertEquals(resolve(url).func.view_class, ProductDetailView)
