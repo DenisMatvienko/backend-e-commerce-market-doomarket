ca64312a8a17cfdc51a1722d90beed64a939082f
Denis <grabsomebuds27@gmail.com>
2020-06-29 18:00:21 +0300

    card-top-bot-properties-done

--- doomarket/shop/views.py
+++ doomarket/shop/views.py
@@ -10,12 +10,6 @@ class CategorySubcategory:
         """ Get list of category on main page filter """
         return Category.objects.all()
 
-    def get_properties(self):
-        product_object = Product.objects.all()
-        for property in product_object:
-            result = property.properties.all()
-        return result
-
 
 class ProductListView(ListView, CategorySubcategory):
     """
