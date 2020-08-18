1b674523088dde71ed03edc4e688307dedd6b387
Denis <grabsomebuds27@gmail.com>
2020-08-18 19:56:12 +0300

    didn't match message rewrite

--- doomarket/shop/forms/user_create_form.py
+++ doomarket/shop/forms/user_create_form.py
@@ -4,6 +4,10 @@ from django import forms
 
 
 class CreateUserForm(UserCreationForm):
+    error_messages = {
+        'password_mismatch': 'Поля паролей не совпадают',
+    }
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
